3e2e576aa2387b6c5ecedcb9e72c629acd4b8790
Bill Dang <sparkfire.ffs@gmail.com>
2020-11-08 15:50:36 -0600

    Setup basic IK; install Fast IK; cleanup files

---
+++ Assets/Scripts/LegController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using DitzelGames.FastIK;
+
+public class LegController : MonoBehaviour {
+
+    [Header("IK Control")]
+    public Transform baseTarget;
+    public Transform endTarget;
+
+    private FastIKFabric ik;
+
+    private void Awake() {
+        ik = GetComponentInChildren<FastIKFabric>();
+    }
+
+    private void LateUpdate() {
+        if(baseTarget)
+            transform.position = baseTarget.position;
+        ik.Target = endTarget;
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(endTarget.position, 0.05f);
+    }
+
+}
