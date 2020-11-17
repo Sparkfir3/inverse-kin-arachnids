6c100f6334884413ed3c9369e4d28e66a320e92a
Cameron <cameron.meyer02@gmail.com>
2020-11-17 02:55:35 -0600

    Basic Step Functionality, Prefab Updates, Spider Body Models
    Covers steps 2-7 of the video tutorial

--- Assets/Scripts/LegController.cs
+++ Assets/Scripts/LegController.cs
@@ -7,12 +7,18 @@ using DitzelGames.FastIK;
 public class LegController : MonoBehaviour {
 
     [Header("IK Control")]
-    public Transform endTarget;
+    public Transform endTarget;             //Position where the foot will try to place itself
+    public Transform targetCaster;          //Floating transform where the raycast will originate from
+    public float distThreshold = 1.0f;      //Max distance between raycast hit and current foot position before spider takes a step
+    public float stepTime = 0.5f;           //Time required for the leg to take a step forward/backward
 
     private bool _active;
     private FastIKFabric ik;
     private Rigidbody rb;
     private List<BoxCollider> boxColliders = new List<BoxCollider>();
+    private RaycastHit hit;
+    private bool drawRayGizmo = false;
+    private Vector3 footStopPos;
 
     // -------------------------------------------------------------------------------------------------------------
 
@@ -21,6 +27,7 @@ public class LegController : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
         foreach(BoxCollider collider in GetComponentsInChildren<BoxCollider>())
             boxColliders.Add(collider);
+        footStopPos = new Vector3(-2, 0, 0);
     }
 
     // -------------------------------------------------------------------------------------------------------------
@@ -51,7 +58,31 @@ public class LegController : MonoBehaviour {
         endTarget.position = position;
     }
 
-    // -------------------------------------------------------------------------------------------------------------
+    private void Update()
+    {
+        SetTarget(footStopPos);
+
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(targetCaster.transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, LayerMask.GetMask("Default")))
+        {
+            float travelDistance = Vector3.Distance(hit.point, endTarget.transform.position);
+            Debug.DrawRay(targetCaster.transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.magenta);
+            Debug.DrawRay(hit.point, transform.TransformDirection(Vector3.left) * travelDistance, Color.cyan);
+            Debug.Log("Did Hit");
+            drawRayGizmo = true;
+
+            if(travelDistance > distThreshold)
+            {
+                StartCoroutine(StepLerp(stepTime));
+            }
+        }
+        else
+        {
+            Debug.DrawRay(targetCaster.transform.position, transform.TransformDirection(Vector3.down) * 1000, Color.red);
+            Debug.Log("Did not Hit");
+            drawRayGizmo = false;
+        }
+    }
 
     private void LateUpdate() {
         if(ik.enabled)
@@ -59,8 +90,31 @@ public class LegController : MonoBehaviour {
     }
 
     private void OnDrawGizmos() {
+        //End Target Gizmo (RED)
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(endTarget.position, 0.05f);
+
+        //Target Caster Gizmo (BLUE)
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(targetCaster.position, 0.1f);
+
+        if(drawRayGizmo)
+        {
+            //Raycast Hit Gizmo (GREEN)
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(hit.point, 0.1f);
+        }
     }
 
+    IEnumerator StepLerp(float stepTime)
+    {
+        Vector3 oldFootPos = footStopPos;
+        Vector3 newFootPos = hit.point;
+
+        for (float l = 0; l < 1; l += (stepTime * Time.deltaTime))
+        {
+            footStopPos = Vector3.Lerp(oldFootPos, newFootPos, l);
+            yield return null;
+        }
+    }
 }
