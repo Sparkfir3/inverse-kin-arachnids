1d3f3ca8f2d6d45a748ef9bc1e77188cd82684cb
Bill Dang <sparkfire.ffs@gmail.com>
2020-11-21 19:29:48 -0600

    Updates to fireball, camera movement, leg spawning, and more

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 1.5f;
+    [SerializeField] float moveSpeed = 10f;
     private Vector3 moveDirection = Vector3.zero;
 
     Vector3 Angles;
     [SerializeField] float sensitivityVertical = 2.0f;
     [SerializeField] float sensitivityHorizontal = 2.0f;
+
     void Update()
     {
         float rotationY = Input.GetAxis("Mouse Y") * sensitivityVertical;
@@ -25,7 +26,8 @@ public class CameraController : MonoBehaviour
     {
         moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         moveDirection = transform.TransformDirection(moveDirection);
-        moveDirection *= moveSpeed;
+        moveDirection += new Vector3(0, Input.GetAxis("Jump"), 0);
+        moveDirection *= moveSpeed * Time.deltaTime;
         transform.position += moveDirection;
     }
 }
